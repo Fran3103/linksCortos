47d658682ae46788002c1b478e08d6127e09efb5
Fran3103 <fn.aguirre@hotmail.com>
2025-05-19 18:36:43 -0300

    configuracion h2 en memoria

--- src/test/java/com/AcortaLink/acortaLink/AcortaLinkApplicationTests.java
+++ src/test/java/com/AcortaLink/acortaLink/AcortaLinkApplicationTests.java
@@ -2,8 +2,10 @@ package com.AcortaLink.acortaLink;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AcortaLinkApplicationTests {
 
 	@Test
